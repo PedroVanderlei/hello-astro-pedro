161af4bc290fef06000356eb8a557a2f6cfe4567
Pedro Hemrique Peruzzi Vanderlei <pedrohpvanderlei@hotmail.com>
2026-01-27 19:07:45 -0300

    WIP: estado atual antes de mudar a identidade visual

--- src/config.ts
+++ src/config.ts
@@ -30,8 +30,8 @@ export const SiteMetadata = {
   buildTime: new Date()
 }
 
-export { default as Logo } from './assets/svg/astro/astro-icon-dark.svg'
-export { default as LogoImage } from './assets/astro/astro-logo-dark.png'
+export { default as Logo } from './assets/astro/logo_minima.png'
+export { default as LogoImage } from './assets/astro/logo_minima.png'
 export { default as FeaturedSVG } from './assets/svg/undraw/undraw_design_inspiration.svg'
 export { default as DefaultSVG } from './assets/svg/undraw/undraw_my_feed.svg'
 export { default as DefaultImage } from './assets/undraw/undraw_my_feed.png'
